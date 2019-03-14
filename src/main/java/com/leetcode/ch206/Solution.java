abd04ff1fc8fce1eef47d42d85a1242ccbe7c174
zzp <631757444@qq.com>
2019-03-14 22:14:37 +0800

    更新206题

---
+++ src/main/java/com/leetcode/ch206/Solution.java
@@ -0,0 +1,28 @@
+package com.leetcode.ch206;
+
+
+public class Solution {
+
+      public static class ListNode {
+      int val;
+     ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public  ListNode reverseList(ListNode head) {
+
+        ListNode result = null;
+
+
+        while (head != null){
+            ListNode temp = head.next;
+            head.next = result;
+            result = head;
+            head = temp;
+        }
+
+        return result.next;
+    }
+
+
+}
