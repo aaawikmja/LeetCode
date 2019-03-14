1e8e087daa01411dcc186af673be7d9fa0ee112b
zzp <631757444@qq.com>
2019-03-14 14:21:02 +0800

    更新至第三题

---
+++ src/main/java/com/leetcode/ch02/Solution.java
@@ -0,0 +1,61 @@
+package com.leetcode.ch02;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+ 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+ 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ 示例：
+
+ 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ 输出：7 -> 0 -> 8
+ 原因：342 + 465 = 807
+ */
+public class Solution {
+
+    public  class ListNode{
+             int val;
+   ListNode next;
+    ListNode(int x) { val = x; }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result = new ListNode(0);
+        ListNode temp = result;
+
+        ListNode p1 = l1,p2 = l2;
+        int sum = 0;
+
+
+        while(p1 != null || p2 != null) {
+
+
+
+            if(p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+
+            if(p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            temp.next = new ListNode( sum%10);
+            sum /= 10;
+            temp = temp.next;
+
+        }
+
+        if(sum == 1) {
+            temp.next = new ListNode( 1);
+        }
+
+
+        return result.next;
+    }
+}
+
+
---
+++ src/main/java/com/leetcode/ch03/Solution.java
@@ -0,0 +1,56 @@
+package com.leetcode.ch03;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+ 示例 1:
+
+ 输入: "abcabcbb"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ 示例 2:
+
+ 输入: "bbbbb"
+ 输出: 1
+ 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ 示例 3:
+
+ 输入: "pwwkew"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        List<Character> list = new ArrayList<>();
+
+        int length = 0;
+
+        for(int i = 0;i < s.length();i++){
+
+            if(list.contains(s.charAt(i))){
+
+                length = Math.max(length,list.size());
+
+                int index = list.indexOf(s.charAt(i));
+                list = new ArrayList<>(list.subList(index,list.size()));
+                list.remove(0);
+                list.add(s.charAt(i));
+            }else{
+                list.add(s.charAt(i));
+            }
+
+        }
+        length = Math.max(length,list.size());
+        return length;
+
+    }
+
+}
