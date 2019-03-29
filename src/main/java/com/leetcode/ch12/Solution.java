51cb9b70b2d46fe21af1353aec75344f35a2cf74
zzp <631757444@qq.com>
2019-03-29 12:29:59 +0800

    12题的解法

--- src/main/java/com/leetcode/ch12/Solution.java
+++ src/main/java/com/leetcode/ch12/Solution.java
@@ -72,6 +72,10 @@ public class Solution {
     }
 
 
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.intToRoman(3999 ));
+    }
 
 
 
